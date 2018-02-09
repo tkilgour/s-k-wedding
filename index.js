7a533c9bbf8a922f62c3245129c098e37eb7a953
ThomasKilgour <thomas.kilgour@interadcorp.com>
2018-02-09 16:50:08 -0500

    add basic auth and some styling

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const basicAuth = require("express-basic-auth");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config();
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(
+		basicAuth({
+			users: { admin: process.env.AUTH_PASS },
+			challenge: true
+		})
+	)
+	.use(express.static(path.join(__dirname, "public")))
+	.set("views", path.join(__dirname, "views"))
+	.set("view engine", "ejs")
+	.get("/", (req, res) => res.render("pages/index"))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
