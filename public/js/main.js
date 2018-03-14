0f7e61c6428e58b6b1478efce4b0d1d38fab4854
ThomasKilgour <t.kilgour@gmail.com>
2018-03-14 14:02:21 -0400

    refactor data model and add error handling

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,42 @@
-// window.onload = function() {
-  new Vue({
-    el: "#rsvp",
-    data: {
-      party_name: "",
-      guests: [],
-      max_guests: 0,
-      loading: true,
-    },
-    methods: {
-      addGuest: function(e) {
-        if (this.guests.length < this.max_guests) {
-          var guest = {
-            first_name: "",
-            last_name: "",
-            camping: false,
-            breakfast: false,
-            dietary: "",
-            temp_added: true
-          };
-          this.guests.push(guest);
-        }
-      },
-      removeGuest: function(i) {
-        this.guests.splice(i, 1);
+new Vue({
+  el: "#rsvp",
+  data: {
+    party: null,
+    loading: true
+  },
+  methods: {
+    addGuest: function(e) {
+      if (this.party.guests.length < this.party.max_guests) {
+        var guest = {
+          first_name: "",
+          last_name: "",
+          camping: false,
+          breakfast: false,
+          dietary: "",
+          temp_added: true
+        };
+        this.party.guests.push(guest);
       }
     },
-    computed: {
-      isDisabled() {
-        return this.guests.length >= this.max_guests;
-      }
+    removeGuest: function(i) {
+      this.party.guests.splice(i, 1);
     },
-    mounted() {
-      axios
-        // ASSUMPTION: the pathname will always be "/{party_slug}"
-        .get("http://localhost:5001/api/" + location.pathname)
-        .then(res => {
-          let party = res.data[0]
-
-          this.party_name = party.party_name;
-          this.guests = party.guests;
-          this.max_guests = party.max_guests;
-          this.loading = false;
-        })
+    submit: function(e) {
+      
     }
-  });
-// };
+  },
+  computed: {
+    isDisabled() {
+      return this.party.guests.length >= this.party.max_guests;
+    }
+  },
+  mounted() {
+    axios
+      // ASSUMPTION: the pathname will always be "/{party_slug}"
+      .get("http://localhost:5001/api/" + location.pathname)
+      .then(res => {
+        if (res.data.length > 0) this.party = res.data[0];
+        this.loading = false;
+      })
+  }
+});
